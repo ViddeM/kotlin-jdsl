31503e7f07087d0b2eccaf079dd137a80f92e837
Vidar Magnusson <vmagnus3@volvocars.com>
2023-04-27 09:29:24 +0200

    Add support for jointype + predicate
    Add support for left/right joining whilst also having an "on" clause

--- query/src/main/kotlin/com/linecorp/kotlinjdsl/querydsl/from/JoinDsl.kt
+++ query/src/main/kotlin/com/linecorp/kotlinjdsl/querydsl/from/JoinDsl.kt
@@ -1,6 +1,10 @@
 package com.linecorp.kotlinjdsl.querydsl.from
 
+import com.linecorp.kotlinjdsl.query.spec.expression.ColumnSpec
 import com.linecorp.kotlinjdsl.query.spec.expression.EntitySpec
+import com.linecorp.kotlinjdsl.query.spec.predicate.EqualExpressionSpec
+import com.linecorp.kotlinjdsl.query.spec.predicate.EqualValueSpec
+import com.linecorp.kotlinjdsl.query.spec.predicate.OrSpec
 import com.linecorp.kotlinjdsl.query.spec.predicate.PredicateSpec
 import javax.persistence.criteria.JoinType
 import kotlin.reflect.KClass
@@ -8,6 +12,17 @@ import kotlin.reflect.KClass
 interface JoinDsl : AssociateDsl, TreatDsl {
     fun on(predicate: PredicateSpec): PredicateSpec = predicate
     fun on(predicate: () -> PredicateSpec): PredicateSpec = on(predicate())
+
+    fun <T> on(joinColA: ColumnSpec<T>, joinColB: ColumnSpec<T>, joinType: JoinType): PredicateSpec {
+        val nullableColA = ColumnSpec<T?>(joinColA.entity, joinColA.path)
+        val nullableColB = ColumnSpec<T?>(joinColB.entity, joinColB.path)
+        val colsEqual = EqualExpressionSpec(joinColA, joinColB)
+        return when (joinType) {
+            JoinType.INNER ->  colsEqual
+            JoinType.LEFT -> OrSpec(listOf(colsEqual, EqualValueSpec(nullableColB, null)))
+            JoinType.RIGHT -> OrSpec(listOf(colsEqual, EqualValueSpec(nullableColA, null)))
+        }
+    }
 
     fun <T, R> join(
         left: EntitySpec<T>,
